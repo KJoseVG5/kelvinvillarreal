0f8c953028e3824606e3f54a11e1bb4fbf28cd4e
Kelvin Villarreal Godoy <69981840+kelvinvillarreal@users.noreply.github.com>
2020-08-21 01:32:46 +0200

    Delete BudgetRepository.java
    borrar

--- mybudget-budget/src/main/java/br/com/kelvinvillarreal/mybudget/budget/BudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.kelvinvillarreal.mybudget.budget;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BudgetRepository extends CrudRepository<Budget, Long> {
-	
-}
